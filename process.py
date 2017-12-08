715858c40c5475b329eac2cbbfab6c99da1f5cbf
Pkataki <paulogkataki@hotmail.com>
2017-12-08 00:02:03 -0200

    arrumei o broadcast para o proprio processo

--- process.py
+++ process.py
@@ -8,23 +8,9 @@ ips = ['localhost','localhost','localhost']
 ports = [5005, 5006, 5007]
 
 
-def make_request(num_process, t):
-	tup1 = ("wanted", num_process, t)
-	for i in range(0,num_process-1):
-		if i == num_process:
-			continue
-		addr = (('127.0.0.1',ports[i])) 
-		client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-		client_socket.connect(addr)
 
-		client_socket.send(bytes(tup1))
-		state = client_socket.recv(1024)
-		if state[0]  == "held"
-			return False;
-		else if state[0] == "wanted" and 
-		client_socket.close()
-	return True
- 
+def listen_process(num_process) : 
+
 
 
 
@@ -42,10 +28,28 @@ class process:
 		self.state = state
 	def set_time_stamp(self, time_stamp):
 		self.time_stamp = time_stamp
+	def make_request(self, t):
+		tup1 = ("wanted", number_process, t)
+		for i in range(0,self.num_process-1):
+			if i == self.num_process:
+				continue
+			addr = (('127.0.0.1',ports[i])) 
+			client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+			client_socket.connect(addr)
+
+			client_socket.send(bytes(tup1))
+			state = client_socket.recv(1024)
+			if state[0]  == "held"
+				return False;
+			else if state[0] == "wanted" and  
+			client_socket.close()
+		return True
 	def enter_critical_region(self):
 		self.state = "wanted"
 		t = random.radint()
 		if make_request(self.num_process, t)  == True : 
 			state = "held"
+			
+
 
 
