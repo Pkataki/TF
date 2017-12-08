f7afe7f03fe96af24175f6b6e5dc3464a8c57ecf
GuilhermeCoutinho <guilhermejcoutinho@gmail.com>
2017-12-08 00:22:10 -0200

    Initial clock logic

--- process.py
+++ process.py
@@ -7,17 +7,15 @@ number_process = 3
 ips = ['localhost','localhost','localhost']
 ports = [5005, 5006, 5007]
 
-
-
-def listen_process(num_process) : 
-
-
-
-
 class process:
+	clock
 	time_stamp
 	state = "released"
 	num_process
+
+	def __init__(self):
+    	self.clock = 0
+
 	def set_num_preocess(self, num_process):
 		self.num_process = num_process
 	def get_state(self):
@@ -28,6 +26,10 @@ class process:
 		self.state = state
 	def set_time_stamp(self, time_stamp):
 		self.time_stamp = time_stamp
+
+	def on_message_received (self , time_stamp):
+		self.clock = max (self.clock + 1 , time_stamp + 1 )
+
 	def make_request(self, t):
 		tup1 = ("wanted", number_process, t)
 		for i in range(0,self.num_process-1):
@@ -44,6 +46,7 @@ class process:
 			else if state[0] == "wanted" and  
 			client_socket.close()
 		return True
+
 	def enter_critical_region(self):
 		self.state = "wanted"
 		t = random.radint()
