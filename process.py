09c516d8cc62b82375b687f1171dc7771fd223d1
Pkataki <paulogkataki@hotmail.com>
2017-12-08 00:46:37 -0200

    arrumei o broadcast para o proprio processo

--- process.py
+++ process.py
@@ -1,50 +1,54 @@
 import socket
 import time
+import thread
 
 number_process = 3
 
 ips = ['localhost','localhost','localhost']
 ports = [5005, 5006, 5007]
+	
 
 class process:
-	clock
-	time_stamp
+	clock = 0
+	time_stamp = 0
 	state = "released"
-	num_process
+	num_process = 0
 
 	def __init__(self):
-    	self.clock = 0
+		self.clock = 0
 
 	def set_num_process(self, num_process):
 		self.num_process = num_process
 		
 	def get_state(self):
 		return self.state
+	
 	def get_time_stamp(self):
 		return self.time_stamp;
+	
 	def set_state(self, state):
 		self.state = state
+	
 	def set_time_stamp(self, time_stamp):
 		self.time_stamp = time_stamp
 
 	def on_message_received (self , time_stamp):
 		self.clock = max (self.clock + 1 , time_stamp + 1 )
 
-	def make_request(self, t):
-		tup1 = ("wanted", self.num_process, t)
-		for i in range(0,number_process-1):
-			if i == self.num_process:
-				continue
-			addr = (('127.0.0.1',ports[i])) 
-			client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-			client_socket.connect(addr)
-
-			client_socket.send(bytes(tup1))
-			state = eval(client_socket.recv(1024))
-			if state[0]  == "held"
-				return False; 
-			client_socket.close()
-		return True
+	# def make_request(self, t):
+	# 	tup1 = ("wanted", self.num_process, t)
+	# 	for i in range(0,number_process-1):
+	# 		if i == self.num_process:
+	# 			continue
+	# 		addr = (('127.0.0.1',ports[i])) 
+	# 		client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+	# 		client_socket.connect(addr)
+	# 		client_socket.send(bytes(tup1))
+	# 		state = eval(client_socket.recv(1024))
+	# 		if state[0]  == "held":
+	# 			return False; 
+	# 		client_socket.close()
+	# 	return True
 
 	def enter_critical_region(self):
 		self.state = "wanted"
@@ -53,16 +57,12 @@ class process:
 			state = "held"
 		#enter critical region
 		sleep(5)
-	def listen_process(self):
-		connectionSocket, addr = server_socket.accept()
-	    tup1 = eval(connection_socket.recv(1024))
-	 	if 
-	    #tup2 = (self.state,self.num_process, self.time_stamp)
-	    connectionSocket.send(capitalizedSentence)
-	    connectionSocket.close()
+	# def listen_process(self):
+	# 	connection_socket, addr = server_socket.accept()
+	# 	tup1 = eval(connection_socket.recv(1024)
+	#     #tup2 = (self.state,self.num_process, self.time_stamp)
+	#     #connection_socket.send()
+	# 	#connection_socket.close()
 
-
-
-
-
-
+if __name__ == "__main__":
+	print "opa"
